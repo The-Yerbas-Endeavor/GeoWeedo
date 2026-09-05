c7d511896632dde09755337ab8afbbc352d91c94
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-09-04 20:18:44 -0700

    Mount individual visitor analytics in admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,6 +1,8 @@
+import './analytics-users.css';
 import AdminHeaderHomeLink from '@/components/AdminHeaderHomeLink';
 import AdminTreasuryBalance from '@/components/AdminTreasuryBalance';
+import AdminIndividualVisitors from '@/components/AdminIndividualVisitors';
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}<AdminHeaderHomeLink /><AdminTreasuryBalance /></>;
+  return <>{children}<AdminIndividualVisitors/><AdminHeaderHomeLink /><AdminTreasuryBalance /></>;
 }
