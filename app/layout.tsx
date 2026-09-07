3c6b0a4e7ab6345d3476741a89b81346c0728f0d
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-09-07 10:40:28 -0700

    Wrap app with MapLibre worker provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,7 @@ import PendingGameRewardClaim from '@/components/PendingGameRewardClaim';
 import HomeMapUiCleanup from '@/components/HomeMapUiCleanup';
 import MapBrowserPanelDraggable from '@/components/MapBrowserPanelDraggable';
 import SponsoredMapLogoEnhancer from '@/components/SponsoredMapLogoEnhancer';
+import MapLibreWorkerProvider from '@/components/MapLibreWorkerProvider';
 
 export const metadata = {
   title: 'GeoWeedo',
@@ -70,7 +71,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <link rel="preload" href="/assets/geoweedo/geoweedo-map-marker.png" as="image" type="image/png" />
       </head>
-      <body>{children}<SponsoredMapLogoEnhancer/><MapBrowserPanelDraggable/><HomeMapUiCleanup/><PendingGameRewardClaim/><AdminGoogleApiStatus/><AdminAmenitiesCheckboxes/><HomeLocationSelectionCardMinimizer/><DispensaryBrowseTierOrder/><MobileGuessMapController/><StateCandidateStreetViewVerifier/><GeolocationReliability/><AnalyticsTracker/><ProductAnalyticsEvents/><HomeMapLocationDeepLink/><DispensaryCardEnhancer/><FloatingStreetViewEnhancer/><LoadedRegionCoveragePortal/><BrowseCountryPartition/></body>
+      <body><MapLibreWorkerProvider>{children}</MapLibreWorkerProvider><SponsoredMapLogoEnhancer/><MapBrowserPanelDraggable/><HomeMapUiCleanup/><PendingGameRewardClaim/><AdminGoogleApiStatus/><AdminAmenitiesCheckboxes/><HomeLocationSelectionCardMinimizer/><DispensaryBrowseTierOrder/><MobileGuessMapController/><StateCandidateStreetViewVerifier/><GeolocationReliability/><AnalyticsTracker/><ProductAnalyticsEvents/><HomeMapLocationDeepLink/><DispensaryCardEnhancer/><FloatingStreetViewEnhancer/><LoadedRegionCoveragePortal/><BrowseCountryPartition/></body>
     </html>
   );
 }
