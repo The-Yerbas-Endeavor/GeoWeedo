be67b768b776b9cba44ea97de09caa4f6faf2912
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-09-07 18:55:21 -0700

    Add leaderboard to site navigation

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -1,6 +1,7 @@
 export default function SiteHeader() {
   const links = [
     ['/how-to-play', 'How to play'],
+    ['/leaderboard', 'Leaderboard'],
     ['/rewards', 'YERB rewards'],
     ['/for-dispensaries', 'For dispensaries'],
     ['/about', 'About'],
