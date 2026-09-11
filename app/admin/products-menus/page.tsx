7ace37a70b91e8aea51b93b616ab51dd03060037
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-09-10 19:07:46 -0700

    Add products and menus admin page

---
+++ app/admin/products-menus/page.tsx
@@ -0,0 +1,7 @@
+import AdminProductsMenus from '@/components/AdminProductsMenus';
+
+export const dynamic = 'force-dynamic';
+
+export default function ProductsMenusAdminPage(){
+  return <AdminProductsMenus />;
+}
