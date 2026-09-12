9b97f71cc56f427496bb945d6023ddd0505aca8d
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-09-11 17:24:40 -0700

    Add cultivar genetics import page

---
+++ app/admin/cultivar-genetics/import/page.tsx
@@ -0,0 +1,7 @@
+import AdminCultivarImport from '@/components/AdminCultivarImport';
+
+export const dynamic='force-dynamic';
+
+export default function CultivarImportPage(){
+  return <AdminCultivarImport/>;
+}
