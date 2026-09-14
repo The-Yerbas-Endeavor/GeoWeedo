1a9394c32d603543a7316dcc1b277ead7701fde8
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-09-13 18:30:22 -0700

    Add product category manager to admin

--- app/admin/products-menus/page.tsx
+++ app/admin/products-menus/page.tsx
@@ -1,7 +1,8 @@
 import AdminProductsMenus from '@/components/AdminProductsMenus';
+import ProductCategoryManager from '@/components/ProductCategoryManager';
 
 export const dynamic = 'force-dynamic';
 
 export default function ProductsMenusAdminPage(){
-  return <AdminProductsMenus />;
+  return <><AdminProductsMenus/><ProductCategoryManager/></>;
 }
