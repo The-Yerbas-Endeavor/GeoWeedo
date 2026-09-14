e6cdd2fe9605683f9523497bad2392716199da91
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-09-13 17:20:01 -0700

    Respect embedded map scanner placement

--- components/MapOpenNowFilter.tsx
+++ components/MapOpenNowFilter.tsx
@@ -41,7 +41,7 @@ export default function MapOpenNowFilter(){
     scanner.innerHTML='<svg viewBox="0 0 24 24" aria-hidden="true" focusable="false" style="width:18px;height:18px;display:block"><path fill="currentColor" d="M3 3h6v6H3V3Zm2 2v2h2V5H5Zm10-2h6v6h-6V3Zm2 2v2h2V5h-2ZM3 15h6v6H3v-6Zm2 2v2h2v-2H5Zm7-14h2v2h-2V3Zm0 4h2v4h-2V7Zm4 4h2v2h-2v-2Zm4 0h2v4h-2v-4Zm-8 4h2v2h-2v-2Zm4 0h4v2h-2v2h-2v-4Zm-4 4h2v2h-2v-2Zm8 0h2v2h-2v-2Z"/></svg>';
     Object.assign(scanner.style,{display:'inline-flex',alignItems:'center',justifyContent:'center',width:'38px',height:'38px',flex:'0 0 38px',border:'1px solid rgba(255,255,255,.14)',borderRadius:'999px',background:'rgba(24,33,30,.92)',color:'inherit',textDecoration:'none',boxSizing:'border-box',cursor:'pointer'});
    }
-   if(search.nextElementSibling!==scanner)search.insertAdjacentElement('afterend',scanner);
+   if(search.dataset.mapScannerEmbedded!=='1'&&search.nextElementSibling!==scanner)search.insertAdjacentElement('afterend',scanner);
   };
 
   const ensureButton=()=>{
