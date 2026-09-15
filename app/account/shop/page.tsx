7bb2f42ebc2a38fa3eb5c870005abcb7ecc7ded5
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-09-14 19:20:01 -0700

    Place shop dashboard under account tabs

--- app/account/shop/page.tsx
+++ app/account/shop/page.tsx
@@ -1,1 +1,11 @@
-export { default } from '../../owner/page';
+import SiteHeader from '@/components/SiteHeader';
+import AccountWorkspaceTabs from '@/components/AccountWorkspaceTabs';
+import OwnerPage from '../../owner/page';
+
+export default function AccountShopPage(){
+  return <>
+    <SiteHeader/>
+    <AccountWorkspaceTabs/>
+    <OwnerPage/>
+  </>;
+}
