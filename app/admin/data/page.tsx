d2abea31d38b8b391c7f3efc738ec5fa61e9ecc4
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-09-16 10:05:11 -0700

    Remove Cannlytics importer from general data page

--- app/admin/data/page.tsx
+++ app/admin/data/page.tsx
@@ -1,13 +1,11 @@
 import AdminDataManager from '@/components/AdminDataManager';
 import AmsterdamCoffeeshopImporter from '@/components/AmsterdamCoffeeshopImporter';
-import CannlyticsImporterMount from '@/components/CannlyticsImporterMount';
 
 export const metadata={title:'GeoWeedo Admin · Data Import'};
 
 export default function AdminDataPage(){
   return <>
     <AdminDataManager/>
-    <CannlyticsImporterMount/>
     <AmsterdamCoffeeshopImporter/>
   </>;
 }
