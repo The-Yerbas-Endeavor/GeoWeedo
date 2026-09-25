1fc7e2c2b9892ce54fd4ae70d9fa5975d8993e73
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-09-24 18:15:03 -0700

    Use global header on app/account/products/page.tsx

--- app/account/products/page.tsx
+++ app/account/products/page.tsx
@@ -1,11 +1,9 @@
-import SiteHeader from '@/components/SiteHeader';
 import AccountWorkspaceTabs from '@/components/AccountWorkspaceTabs';
 import OwnerProductsPage from '../../owner/products/page';
 
 export default function AccountProductsPage(){
   return <>
-    <SiteHeader/>
-    <AccountWorkspaceTabs/>
+<AccountWorkspaceTabs/>
     <OwnerProductsPage/>
   </>;
 }
